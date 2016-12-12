c0ab89ba2e68222be52a84fcfadf05022c168dff
kgot <phoinikaskg@gmail.com>
2016-12-12 21:37:55 +0200

    Ready for testing data tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
 
 var libs = {
   styled: [
@@ -19,26 +20,28 @@ var sources = {
   ]
 };
 
-var destinations = {
+var dest = {
   js: 'build'
 };
 
-gulp.task('build', function() {
+gulp.task('bundle', function() {
   return gulp.src(sources.files)
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(concat('bundle.js'))
-    .pipe(gulp.dest(destinations.js));
+    .pipe(gulp.dest(dest.js));
 });
 
 gulp.task('vendor', function() {
   return gulp.src(libs.styled)
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(destinations.js))
+    .pipe(gulp.dest(dest.js))
     .pipe(connect.reload());
 });
 
-gulp.task('connect', ['build'], function() {
+gulp.task('connect', ['vendor', 'bundle'], function() {
   var app = connect.server({
     port: 8888,
     livereload: true
@@ -46,11 +49,12 @@ gulp.task('connect', ['build'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['js/*.js']);
+  gulp.watch(['js/*.js', '*.html'], ['bundle']);
 });
 
 gulp.task('default', [
   'vendor',
-  'build',
+  'bundle',
+  'watch',
   'connect'
 ]);
--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
-var url = '';
+var url = 'localhost:8080/...';
 
-var data = fetch(url, {
+// fetch data and render alchemy graph
+var fetchData = fetch(url, {
     method: 'GET',
     mode: 'cors',
     cache: 'default'
   })
   .then(function(response) {
-    return response.json()
-      .then(function(json) {
-
-      });
+    return response.json();
+  })
+  .then(function(datasource) {
+    // set datasource config data
+    // call alchemy
   })
   .catch(function(error) {
     console.error(error);
   });
 
-alchemy.begin({
-  dataSource: {
-    nodes: [{
-      id: 1
-    }, {
-      id: 2
-    }, {
-      id: 3
-    }],
-    edges: [{
-      source: 1,
-      target: 2
-    }, {
-      source: 1,
-      target: 3,
-    }]
-  }
-});
+// simple datasource
+var simpleDatasource = {
+  nodes: [{ id: 1 }, { id: 2 }, { id: 3 }],
+  edges: [{ source: 1, target: 2 }, { source: 1, target: 3, }]
+};
+
+var dataSourceConfig = {
+  dataSource: simpleDatasource
+  // set more config options
+};
+
+var alchemy = new Alchemy(dataSourceConfig);
